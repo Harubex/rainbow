31034e9b777da32ff2c813dd83906f0252c5cc2b
Hannah <hannah@harubex.com>
2023-07-21 10:57:19 -0600

    Adding cloudwatch logging, updating packages.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,33 @@
 import Discord from "discord.js";
 import fetch from "node-fetch";
+import { createLogger, format, transports } from "winston";
+import WCW from "winston-cloudwatch";
 
 import commands from "./commands";
 
 const disc = new Discord.Client();
+const wcw = new WCW({
+  logGroupName: "rainbow-logs",
+  logStreamName: "rainbow-logs-stream",
+  level: "debug",
+  awsOptions: {
+    region: "us-east-2",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY!,
+      secretAccessKey: process.env.AWS_SECRET_KEY!
+    }
+  }
+});
+
+export const log = createLogger({
+  level: "debug",
+  format: format.json(),
+  transports: [wcw, new transports.Console()]
+});
 
 disc.on("ready", async () => {
     if (disc && disc.user) {
-        console.log(`Logged in as ${disc.user.tag}!`);
+        log.debug(`Logged in as ${disc.user.tag}!`);
     }
 });
 
@@ -32,20 +52,23 @@ disc.on("message", async (msg: Discord.Message) => {
                 const response = await commands[cmd](disc, msg, content.substr(cmd.length));
                 await msg.reply(response);
                 const roles = await msg.guild?.roles.fetch()!;
-                roles.cache.forEach(async (role) => {
-                    if (role.members.size === 0) {
-                        try {
-//                            await role.delete();
-                        } catch (e) {
-                            console.log("Error when deleting roles:", e);
-                        }
-                    }
-                });
+                if (roles.cache) {
+                  roles.cache.forEach(async (role) => {
+                      if (role.members.size === 0) {
+                          try {
+  //                            await role.delete();
+                          } catch (e) {
+                            log.error("Error when deleting roles:", e);
+                          }
+                      }
+                  });
+                }
                 return;
             }
         }
     } catch (e) {
         msg.reply(`An error has occurred; please complain to hannah about it: \n\n ${e}`);
+        log.error(e);
     }
     // Spams every message
     //await msg.reply(`I'm unable to read your command. Here's a list of available commands: \n${Object.keys(commands).join(", ")}`);
